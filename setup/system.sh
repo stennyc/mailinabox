f5b40141dda612c11f573f8bf93c69acdb983067
stennyc <stenny@hotmail.com>
2020-02-18 18:12:43 +0800

    Update system.sh

--- setup/system.sh
+++ setup/system.sh
@@ -254,23 +254,23 @@ echo "4"
 if [ -z "${DISABLE_FIREWALL:-}" ]; then
 	# Install `ufw` which provides a simple firewall configuration.
 	apt_install ufw
-
+echo "4-1"
 	# Allow incoming connections to SSH.
 	ufw_allow ssh;
-
+echo "4-2"
 	# ssh might be running on an alternate port. Use sshd -T to dump sshd's #NODOC
 	# settings, find the port it is supposedly running on, and open that port #NODOC
 	# too. #NODOC
 	SSH_PORT=$(sshd -T 2>/dev/null | grep "^port " | sed "s/port //") #NODOC
 	if [ ! -z "$SSH_PORT" ]; then
 	if [ "$SSH_PORT" != "22" ]; then
-
+echo "4-3"
 	echo Opening alternate SSH port $SSH_PORT. #NODOC
 	ufw_allow $SSH_PORT #NODOC
 
 	fi
 	fi
-
+echo "4-4"
 	ufw --force enable;
 fi #NODOC
 echo "5"
